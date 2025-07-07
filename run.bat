c4f72f68b1bd92fb64fdfd50458837a806218ac2
73LIX <chatgai.note@gmail.com>
2025-07-07 14:36:49 +0530

    added executables

---
+++ run.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Running Tag-Manager...
+call venv\Scripts\activate.bat
+python main.py
+pause
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "🔧 Activating virtual environment and launching the app..."
+source venv/bin/activate
+python3 main.py
